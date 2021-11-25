4f75ff6f59058e9f6881184a6b9d9c86ece80359
thatkookooguy <neilkalman@gmail.com>
2021-11-25 13:01:35 +0200

    all the things

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,41 @@
 import * as core from '@actions/core'
+import github from '@actions/github'
 import {wait} from './wait'
+
+run()
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const githubToken = core.getInput('token')
+    await openPRIfHotfix(githubToken)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
 
-run()
+async function openPRIfHotfix(githubToken: string): Promise<void> {
+  const pullRequest = github.context.payload.pull_request
+
+  if (!pullRequest) {
+    core.debug('No pull request found')
+    return
+  }
+
+  const baseBranch = pullRequest.base.ref as string
+  const branch = pullRequest.head.ref as string
+  const isHotfix = branch.startsWith('hotfix/')
+
+  const octokit = github.getOctokit(githubToken)
+  const isPrAlreadyExists = await octokit.rest.pulls.list({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    state: 'open',
+    head: branch,
+    base: baseBranch
+  })
+
+  core.info(isPrAlreadyExists.toString())
+
+  core.setOutput('branch', branch)
+  core.setOutput('isHotfix', isHotfix)
+}
